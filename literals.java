9f1ae6868b29de9f6339c959628769b8207ee86e
MacBook <patelyogesh5040@gmail.com>
2020-11-23 12:37:42 +0530

    Literals

---
+++ literals.java
@@ -0,0 +1,9 @@
+public class literals {
+    public static void main(String[] args) {
+        //Integer Literals for (byte,short,int,long)
+        int x = 10; //Decimal literal
+        int y = 010; // Octal literal
+        int z = 0x10; //Hexa decimal literal 
+        System.out.println(x + "..." + y + "..." + z); // Jvm always return Decimal value.
+    }
+}
