4950340517ea8a1b59a43f8bbcd0b659475a0482
MacBook <patelyogesh5040@gmail.com>
2020-11-24 18:35:34 +0530

    Array

---
+++ array.java
@@ -0,0 +1,20 @@
+public class array {
+    public static void main(String[] args) {
+        //Array 
+
+        //Array declaration
+        //single dimension array
+        int[] a;
+        int []b;
+        int c[];
+
+        //two dimension array declaration
+        int[]  []d;
+        int[] e[];
+        int [][]f;
+        int []g[];
+        int[][] h;
+        int i[][];
+
+    }
+}
