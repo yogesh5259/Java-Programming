b7b9b7dfd5d6ccd2fc020f5935822cbb8035dc71
MacBook <patelyogesh5040@gmail.com>
2020-11-30 18:42:20 +0530

    main method

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
+        //valid 
     public static void main(String[] args) {
-        
+        System.out.println("main method...");
     }
+
+        //Valid
+    // static      public     void     main(String[] args) {
+    //     System.out.println("main method...");
+    // }
+
+        //Invalid
+    //Error: Main method not found in class Main, please define the main method as:
+    // public static void ypmain(String[] args) {
+    //     System.out.println("main method...");
+    // }
 }
