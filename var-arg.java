185bb50bc15375d64ee1ef309ddaf41ad51645cf
MacBook <patelyogesh5040@gmail.com>
2020-11-30 12:17:25 +0530

    var-arg

--- var-arg.java
+++ var-arg.java
@@ -2,6 +2,9 @@ class VarArg {
     public static void main(String[] args) {
         sum(10);
     }
+    public void sum(String[] ... a){
+        
+    }
     public static void sum(int ... a){
         
     }
