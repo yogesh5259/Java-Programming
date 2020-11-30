8192ae2418aa619c8b3d115b8ceadaa29e369025
MacBook <patelyogesh5040@gmail.com>
2020-11-30 17:59:22 +0530

    var-arg

--- var-arg.java
+++ var-arg.java
@@ -1,11 +1,20 @@
 class VarArg {
     public static void main(String[] args) {
-        sum(10);
+        sum(new int[]{10,20,30});
     }
     public void sum(String[] ... a){
         
     }
     public static void sum(int ... a){
-        
+        System.out.println("Inside int...");
     }
+
+    //error: cannot declare both m1(int...) and m1(int[]) in VarArg
+    // public static void m1(int[] a){
+
+    // }
+
+    // public static void m1(int... x){
+
+    // }
 }
