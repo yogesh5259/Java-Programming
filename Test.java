b638776a2b30b121dbb2136ed7ba591dcf0a7c73
MacBook <patelyogesh5040@gmail.com>
2020-12-09 11:15:46 +0530

    main method

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    static{
+        System.out.println("Inside static block!");
+        System.exit(0);
+    }
+    // public static void main(String[] args) {
+    //     System.out.println("Inside main method!");
+    // }
+}
