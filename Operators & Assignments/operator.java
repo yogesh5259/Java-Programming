e8def6ef810beafde32c0f09ffd8c3b54f883a4d
MacBook <patelyogesh5040@gmail.com>
2020-12-12 19:08:10 +0530

    Operators

---
+++ Operators & Assignments/operator.java
@@ -0,0 +1,23 @@
+class operator {
+    public static void main(String[] args) {
+        // final int x = 10;
+        // //x++; // error: cannot assign a value to final variable x
+        // System.out.println(x);
+
+        // boolean x = true;
+        // x++; //error: bad operand type boolean for unary operator '++'
+        // System.out.println(x);
+
+
+        // byte b = 10;
+        // b++;
+        // System.out.println(b); // 11
+
+
+        // byte b = 10;
+        // b = b + 1; //error: incompatible types: possible lossy conversion from int.
+        // System.out.println(b);
+
+        
+    }
+}
