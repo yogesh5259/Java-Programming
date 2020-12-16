ce04116a76c42e54cc7473305e1794fe97d30112
MacBook <patelyogesh5040@gmail.com>
2020-12-16 13:02:10 +0530

    String Concadination Operator

---
+++ Operators & Assignments/StringConcadination.java
@@ -0,0 +1,10 @@
+class StringConcadination {
+    public static void main(String[] args) {
+        String a  = "Yogesh";
+        int b = 10, c = 20, d = 30;
+        System.out.println(a+b+c+d);
+        System.out.println(b+c+d+a);
+        System.out.println(b+c+a+d);
+
+    }
+}
