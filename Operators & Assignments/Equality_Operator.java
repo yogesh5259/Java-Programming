aa659f6bf4edaf1767d67cd0d21c109b41fb1ee8
MacBook <patelyogesh5040@gmail.com>
2020-12-17 11:05:53 +0530

    Equality Operators

---
+++ Operators & Assignments/Equality_Operator.java
@@ -0,0 +1,19 @@
+
+
+public class Equality_Operator {
+    public static void main(String[] args) {
+        Thread t1 = new Thread();
+        Thread t2 = new Thread();
+        Thread t3 = t1;
+        System.out.println(t1 == t2); // false
+        System.out.println(t1 == t3); // true
+
+
+        Object o = new Object();
+        Thread t = new Thread();
+        String s = new String();
+        System.out.println(t == o); // false
+        System.out.println(o == s); // false
+        //System.out.println(s == t); //compile time error:-error: incomparable types: String and Thread
+    }
+}
