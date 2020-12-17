06a97c53768757fdfae7e22d6e8b354aab423d38
MacBook <patelyogesh5040@gmail.com>
2020-12-17 10:46:04 +0530

    Relational Operators

---
+++ Operators & Assignments/Relational_Operators.java
@@ -0,0 +1,10 @@
+class Relational_Operators{
+    public static void main(String[] args) {
+        System.out.println(10 < 20); // true
+    System.out.println('a' < 10); //false
+    System.out.println('a' < 97.6);  //true
+    System.out.println('a' > 'A');  //true
+    //System.out.println(true > false); //compile time error:- error: bad operand types for binary operator '>'
+    //System.out.println("Yogesh123" > "Yogesh");  //compille time error:-  error: bad operand types for binary operator '>'
+    }
+}
