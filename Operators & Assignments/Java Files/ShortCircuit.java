037807698fb9a2b4ca53a63ef06b6ac48fb43205
MacBook <patelyogesh5040@gmail.com>
2020-12-18 19:09:59 +0530

    Short circuit

--- Operators & Assignments/Java Files/ShortCircuit.java
+++ Operators & Assignments/Java Files/ShortCircuit.java
@@ -5,17 +5,24 @@ class ShortCircuit{
         int x = 10;
         int y = 15;
 
+        if(++x < 10 && (x/0 > 10)){
+            System.out.println("Hello");
+        } else{
+            System.out.println("HI"); //HI
+        }
+
+
         // if(++x < 10 && (x/0 > 10)){
         //     System.out.println("Hello");
         // } else{
         //     System.out.println("HI");
         // }
 
-        if(++x < 10 || ++y > 15){
-            x++;
-        } else {
-            y++;
-        }
-        System.out.println(x + "..." + y); // 12...16
+        // if(++x < 10 || ++y > 15){
+        //     x++;
+        // } else {
+        //     y++;
+        // }
+        // System.out.println(x + "..." + y); // 12...16
     }
 }
