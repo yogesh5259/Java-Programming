36a763eba538190a10aa29c82253c2734daccfe1
MacBook <patelyogesh5040@gmail.com>
2020-12-18 19:04:05 +0530

    short circuit operator

--- Operators & Assignments/Java Files/ShortCircuit.java
+++ Operators & Assignments/Java Files/ShortCircuit.java
@@ -3,10 +3,19 @@
 class ShortCircuit{
     public static void main(String[] args) {
         int x = 10;
-            if(++x < 10 && (x/0 > 10)){
-                System.out.println("Hello");
-            } else{
-                System.out.println("HI");
-            }
+        int y = 15;
+
+        // if(++x < 10 && (x/0 > 10)){
+        //     System.out.println("Hello");
+        // } else{
+        //     System.out.println("HI");
+        // }
+
+        if(++x < 10 || ++y > 15){
+            x++;
+        } else {
+            y++;
+        }
+        System.out.println(x + "..." + y); // 12...16
     }
 }
