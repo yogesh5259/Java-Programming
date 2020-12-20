f09680a0c08a8c06e4b0ddf66db74564b7886c90
MacBook <patelyogesh5040@gmail.com>
2020-12-20 10:56:58 +0530

    Conditional Operator

---
+++ Operators & Assignments/Java Files/ConditionalOperator.java
@@ -0,0 +1,10 @@
+class ConditionalOperator {
+    public static void main(String[] args) {
+
+        int x = (10 < 20) ? 30 : 40;
+        System.out.println(x); // 30
+
+    //     int x = (10 > 20) ? 30 : (40 < 50) ? 60 : 70; 
+    //     System.out.println(x); // 60
+     }
+}
