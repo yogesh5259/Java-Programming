29275601e7d404b5f2e51546dcd4b2106d2c6b69
MacBook <patelyogesh5040@gmail.com>
2020-12-20 10:43:26 +0530

    Assignment operator

---
+++ Operators & Assignments/Java Files/AssignmentOperator.java
@@ -0,0 +1,14 @@
+class AssignmentOperator{
+    public static void main(String[] args) {
+        // int a,b,c,d;
+        // a = b = c = d = 20;
+        // System.out.println(a+"..."+b+"..."+c+"..."+d);//20...20...20...20
+
+        //int a = b = c = d = 20;//cannot find symbol b, c, d
+
+        // int b,c,d;
+        // int a = b= c= d =20;
+        // System.out.println(a+"..."+b+"..."+c+"..."+d);//20...20...20...20
+
+    }
+}
