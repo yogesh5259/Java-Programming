45cd9f2ea1ccf3954755bc004d798b15e8ef6598
MacBook <patelyogesh5040@gmail.com>
2020-12-21 11:50:44 +0530

    Flow Control IF

---
+++ Flow Control/Java File/IfElse.java
@@ -0,0 +1,20 @@
+
+public class IfElse {
+    public static void main(String[] args) {
+        // int x;
+        // if(true) ;
+
+        // int x;
+        // if(true) x = 20;
+
+        int x =10;
+        int y =20;
+        if(true)
+            System.out.println("Hello");
+            if(x > y){
+                System.out.println("10");
+            } else
+            System.out.println("20");
+        
+    }
+}
