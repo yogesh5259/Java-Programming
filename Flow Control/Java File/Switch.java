c532afca6d4c064b4b4d2e586ea3184822476631
MacBook <patelyogesh5040@gmail.com>
2020-12-22 18:56:54 +0530

    switch

--- Flow Control/Java File/Switch.java
+++ Flow Control/Java File/Switch.java
@@ -4,5 +4,63 @@ class Switch {
         // switch(x){
         //     System.out.println("Hello"); //case, default, or '}' expected
         // }
+
+        // int x = 10;
+        // int y = 20;
+        // switch(x){
+        //     case 10:
+        //         System.out.println(10);
+        //         break;
+        //     case y:                     //constant expression required
+        //         System.out.println(20); 
+        //         break;
+        // }
+
+
+        // int x = 10;
+        // final int y = 20;
+        // switch(x){
+        //     case 10:
+        //         System.out.println(10); //10
+        //         break;
+        //     case y:
+        //         System.out.println(20);
+        //         break;
+        // }
+
+
+        // byte b = 10;                                    
+        // switch(b){                                      
+        //     case 10:
+        //         System.out.println(10);
+        //         break;
+
+        //     case 100:
+        //         System.out.println(100);
+        //         break;
+
+        //     case 1000:                  //possible lossy conversion from int to byte
+        //         System.out.println(1000);
+        //         break;
+        // }  
+
+        // int x = 10;                                    
+        //     switch(x){                                             
+        //         case 97:
+        //             System.out.println(97);
+        //             break;
+
+        //         case 98:
+        //             System.out.println(98);
+        //             break;
+
+        //         case 99:
+        //             System.out.println(99);
+        //             break;
+                
+        //         case 'a':                           //duplicate case label
+        //             System.out.println('a');
+        //             break;
+        //     } 
     }
 }
