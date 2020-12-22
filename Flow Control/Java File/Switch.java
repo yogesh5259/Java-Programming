58c5ca051c4f6403605c7e4b9b577d053c76a942
MacBook <patelyogesh5040@gmail.com>
2020-12-22 18:35:33 +0530

    switch

---
+++ Flow Control/Java File/Switch.java
@@ -0,0 +1,8 @@
+class Switch {
+    public static void main(String[] args) {
+        // int x = 10;
+        // switch(x){
+        //     System.out.println("Hello"); //case, default, or '}' expected
+        // }
+    }
+}
