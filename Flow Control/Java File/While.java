03afc48a679ca7498b9a108169db59cac6c1913c
MacBook <patelyogesh5040@gmail.com>
2020-12-23 18:32:15 +0530

    while loop

---
+++ Flow Control/Java File/While.java
@@ -0,0 +1,11 @@
+class While {
+    public static void main(String[] args) {
+        // while(true)
+        //     int x = 10; //variable declaration not allowed here
+    
+        while(true){
+            System.out.println("Hello");
+        }
+        System.out.println("Hi");
+    }
+}
