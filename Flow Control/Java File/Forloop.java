bbc8f2de38a97699bc3eae19ec9c2f54e7fff19c
MacBook <patelyogesh5040@gmail.com>
2020-12-26 19:07:43 +0530

    for loop

---
+++ Flow Control/Java File/Forloop.java
@@ -0,0 +1,11 @@
+class Forloop {
+    public static void main(String[] args) {
+        int i = 0;
+            // for(System.out.println("Hello"); i<3 ; i++){
+            //     System.out.println("HI!!");
+            // }
+            for( ; i<3 ; System.out.println("Hello")){
+                i++;
+            }
+    }
+}
