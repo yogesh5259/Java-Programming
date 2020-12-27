6eb95cd07f96d1fad7f8a3597cdf291578ef8df0
MacBook <patelyogesh5040@gmail.com>
2020-12-27 17:59:42 +0530

    Break

---
+++ Flow Control/Java File/BreakContinue.java
@@ -0,0 +1,23 @@
+class BreakContinue {
+    public static void main(String[] args) {
+
+        // int x = 10;
+        // if(x == 10){
+        //     break;  //break outside switch or loop
+        // }
+
+
+
+
+        // int i = 5;
+        // l1:
+        // {
+        //     System.out.println("Inside block");
+        //     if(i == 5){
+        //         break l1;
+        //     }
+        //     System.out.println("end of block");
+        // }
+        // System.out.println("Outside block");
+    }
+}
