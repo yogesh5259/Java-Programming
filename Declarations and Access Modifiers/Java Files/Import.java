ac9d200a194e348424aee078815403c64b4d129a
MacBook <patelyogesh5040@gmail.com>
2021-01-02 19:49:59 +0530

    import Statement

---
+++ Declarations and Access Modifiers/Java Files/Import.java
@@ -0,0 +1,7 @@
+
+public class Import {
+    public static void main(String[] args){
+        //ArrayList list = new ArrayList();
+        java.util.ArrayList l = new java.util.ArrayList();
+    }
+}
