8d7bc31851b616138a5ab4b8649f55c4cb09a524
MacBook <patelyogesh5040@gmail.com>
2021-01-11 17:26:07 +0530

    import

--- Declarations and Access Modifiers/Java Files/Import.java
+++ Declarations and Access Modifiers/Java Files/Import.java
@@ -1,7 +1,11 @@
+import java.sql.*;
+import java.util.*;
 
 public class Import {
     public static void main(String[] args){
         //ArrayList list = new ArrayList();
         java.util.ArrayList l = new java.util.ArrayList();
+       // Date date = new Date(); // reference to Date is ambiguous
+
     }
 }
