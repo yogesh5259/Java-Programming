979c524bd8211357ced365d06effe5c20f477e9d
MacBook <patelyogesh5040@gmail.com>
2021-01-12 19:29:31 +0530

    import

--- Declarations and Access Modifiers/Java Files/Import.java
+++ Declarations and Access Modifiers/Java Files/Import.java
@@ -1,23 +1,27 @@
 import static java.lang.Math.sqrt;
 import static java.lang.Math.*;
+import static java.lang.System.out;
 
 
 import java.util.Date;
 
 public class Import {
     public static void main(String[] args){
-        //ArrayList list = new ArrayList();
-        java.util.ArrayList l = new java.util.ArrayList();
-       // Date date = new Date(); // reference to Date is ambiguous
+    //     //ArrayList list = new ArrayList();
+    //     java.util.ArrayList l = new java.util.ArrayList();
+    //    // Date date = new Date(); // reference to Date is ambiguous
 
-       Date date = new Date();
-       System.out.println(date.getClass().getName()); //java.util.Date
+    //    Date date = new Date();
+    //    System.out.println(date.getClass().getName()); //java.util.Date
 
 
-        System.out.println(sqrt(4));
-        System.out.println(max(4,3));
-        System.out.println(random());
+    //     System.out.println(sqrt(4));
+    //     System.out.println(max(4,3));
+    //     System.out.println(random());
 
+
+        out.println("Hello");
+        out.println("Hi");
 
     }
 }
