faba0914311618dc66587955909fe9d96f2b843e
MacBook <patelyogesh5040@gmail.com>
2021-01-18 19:52:27 +0530

    final method

--- Declarations and Access Modifiers/Java Files/ClassLevelModifier.java
+++ Declarations and Access Modifiers/Java Files/ClassLevelModifier.java
@@ -1,5 +1,21 @@
-private class ClassLevelModifier {
-    public static void main(String[] args) {
-        System.out.println("Hello!!");
-    }
-}
+// private class ClassLevelModifier {
+//     public static void main(String[] args) {
+//         System.out.println("Hello!!");
+//     }
+// }
+
+
+
+// class p {
+//     public void property(){
+//         System.out.println("Gold");
+//     }
+//     public final void marrie(){
+//         System.out.println("sanju");
+//     }
+// }
+// class c extends p {
+//     public void marrie(){
+//         System.out.println("xyz");
+//     }
+// }
