7c436b5e28725d121ce79fb5ee3248334320e29e
MacBook <patelyogesh5040@gmail.com>
2021-01-29 20:04:34 +0530

    final static variables

--- Declarations and Access Modifiers/Java Files/MemberLevelModifier.java
+++ Declarations and Access Modifiers/Java Files/MemberLevelModifier.java
@@ -1,7 +1,12 @@
 class Test {
-    final int x;
+    // final int x;
     
-    public void m1(){
-        x = 10;
+    // public void m1(){
+    //     x = 10;
+    // }
+
+    final static int x;
+    static {
+        x = 0;
     }
 }
