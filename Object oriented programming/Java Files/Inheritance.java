7b972c36a64c26c77c6284fc5e26aac37e287b95
MacBook <patelyogesh5040@gmail.com>
2021-02-09 20:11:16 +0530

    Is - A relationship

---
+++ Object oriented programming/Java Files/Inheritance.java
@@ -0,0 +1,34 @@
+class P{
+    public void m1(){
+        System.out.println("Parent");
+    }
+}
+
+class C extends P{
+    public void m2(){
+        System.out.println("Child");
+    }
+}
+
+class Test {
+    public static void main(String[] args){
+        
+        //case 1
+        P p = new P();
+        p.m1();
+        p.m2();
+
+        //case 2
+        C c = new C();
+        c.m1();
+        c.m2();
+
+        //case 3
+        P p1 = new C();
+        p1.m1();
+        p1.m2();
+
+        //case 4
+        C c1 = new P();
+    }
+}
