ab2b9a77fdfb7e1489de69b936d211d2964418f5
MacBook <patelyogesh5040@gmail.com>
2021-02-14 18:25:35 +0530

    Overriding

--- Object oriented programming/Java Files/Overriding.java
+++ Object oriented programming/Java Files/Overriding.java
@@ -1,27 +1,47 @@
-class Parent{
-    public void property(){
-        System.out.println("Gold");
-    }
-    public void marry(){
-        System.out.println("Mary");
-    }
-}
+import java.io.IOException;
 
-class Child extends Parent {
-    public void marry(){
-        System.out.println("Priyanka");
-    }
-}
+//Case 1
 
-class Test{
-    public static void main(String[] args){
-        Parent p = new Parent();
-        p.marry();
 
-        Child c = new Child();
-        c.marry();
+// class Parent{
+//     public void property(){
+//         System.out.println("Gold");
+//     }
+//     public void marry(){
+//         System.out.println("Mary");
+//     }
+// }
 
-        Parent p1 = new Child();
-        p1.marry();
-    }
-}
+// class Child extends Parent {
+//     public void marry(){
+//         System.out.println("Priyanka");
+//     }
+// }
+
+// class Test{
+//     public static void main(String[] args){
+//         Parent p = new Parent();
+//         p.marry();
+
+//         Child c = new Child();
+//         c.marry();
+
+//         Parent p1 = new Child();
+//         p1.marry();
+//     }
+// }
+
+
+
+    // Case 2
+
+    // class P {
+    //     public void m1() throws IOException{
+
+    //     }
+    // }
+    // class C extends P {
+    //     public void m1() throws EOFException, InterruptedException{
+            
+    //     }
+    // }
