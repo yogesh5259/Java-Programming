17cc3a04555c40fec49fe75ec249a6814e0028a0
MacBook <patelyogesh5040@gmail.com>
2021-04-01 20:14:27 +0530

    Instance control flow

--- Object oriented programming/Java Files/InstanceControlFlow.java
+++ Object oriented programming/Java Files/InstanceControlFlow.java
@@ -1,4 +1,4 @@
-public class InstanceControlFlow{
+/* public class InstanceControlFlow{
     int i = 10;
     {
         m1();
@@ -11,6 +11,7 @@ public class InstanceControlFlow{
     public static void main(String[] args){
         InstanceControlFlow obj = new InstanceControlFlow();
         System.out.println("Main Method");
+        InstanceControlFlow obj1 = new InstanceControlFlow();
     }
     public void m1(){
         System.out.println(j);
@@ -19,4 +20,47 @@ public class InstanceControlFlow{
         System.out.println("Second Instance");
     }
     int j = 20;    
+}
+*/
+
+
+class Parent {
+    int i = 10;
+    {
+        m1();
+        System.out.println("Parent Instance Block");
+    }
+    Parent(){
+        System.out.println("Parent Constructor");
+    }
+    public static void main(String[] args){
+        Parent p = new Parent();
+        System.out.println("Parent main");
+    }
+    public void m1(){
+        System.out.println(j);
+    }
+    int j = 20;
+}
+
+class Child extends Parent{
+    int x = 100;
+    {
+        m2();
+        System.out.println("Child first instance block");
+    }
+    Child(){
+        System.out.println("child Constructor");
+    }
+    public static void main(String[] args){
+        Child  c = new Child();
+        System.out.println("Child Main");
+    }
+    public void m2(){
+        System.out.println(j);
+    }
+    {
+        System.out.println("Child Second Instance Block");
+    }
+    int j = 200;
 }
