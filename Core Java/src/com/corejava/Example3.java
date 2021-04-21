3b1afbf9c2d3fe1b92f900f107ce282b090a81eb
MacBook <patelyogesh5040@gmail.com>
2021-04-21 10:43:33 +0530

    Core Java

---
+++ Core Java/src/com/corejava/Example3.java
@@ -0,0 +1,31 @@
+package com.corejava;
+
+/**
+ *Created by Yogesh Patel on 20/04/2021
+ *Refer Language Fundamentals -> DataTypes.txt
+ *
+ *
+ * Java contain 8 data types
+ * byte, short, int, char, long, float, double, boolean
+ */
+public class Example3 {
+    public static void main(String[] args) {
+        byte b = 123;         //size:- 8 bit
+        short s = 32767;        //size:- 16bit
+        int i = 2147483647;     //size:- 32 bit
+        char c = 'c';           // size:- 16bit (unicode base)
+        long l = 12345L;        // size:- 64bit
+        double d = 10.5;        // size:- 64bit
+        float f = 10.4f;        //size:- 32bit
+        boolean b1 = true;      //depends on virtual machine (JVM)
+
+
+        // data type assignment
+        byte b2 = 127; //decimal value
+        byte b3 = 012; //octal value // 8^1 * 1 + 8^0 * 2  = 10
+        byte b4 = 0x1A; //hex value
+        byte b5 = 0b110;
+        System.out.println(b2 + " " + b3 + " " + b4 + " " + b5);
+
+    }
+}
