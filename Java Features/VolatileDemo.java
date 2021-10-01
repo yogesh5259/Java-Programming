e07501328f37ca7edef75bb926e6c7adf0fd583c
bharat-kadchha <bharatkadchha1@gmail.com>
2021-10-01 11:20:49 +0530

    Java Feature Programming

---
+++ Java Features/VolatileDemo.java
@@ -0,0 +1,19 @@
+
+public class VolatileDemo {
+	
+	Thread t = new Thread(new MyRunnable());
+	
+}
+
+
+class MyRunnable implements Runnable {
+	   private volatile boolean active;
+	   public void run() {
+	      active = true;
+	      while (active) {    
+	      }
+	   }
+	   public void stop() {
+	      active = false;  
+	   }
+}
