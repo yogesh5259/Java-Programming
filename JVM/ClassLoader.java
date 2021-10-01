792d8109c436d05e6875f2b2875a66f0d0a7f5ec
bharat-kadchha <bharatkadchha1@gmail.com>
2021-10-01 10:51:57 +0530

    add JVM programming

---
+++ JVM/ClassLoader.java
@@ -0,0 +1,18 @@
+
+public class ClassLoader {
+
+	static {
+		System.out.println("static bolcok");
+		System.exit(0);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(String.class.getCanonicalName());
+
+		System.out.println(String.class.getClassLoader());
+
+		System.out.println(Bike.class.getClassLoader());
+	}
+
+}
